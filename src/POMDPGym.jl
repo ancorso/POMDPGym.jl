0659b2346b0c179e590c4751e899662abbeb0da6
Anthony Corso <anthonycorso92@gmail.com>
2020-10-27 16:26:37 -0700

    First pass working with pixel observations

--- src/POMDPGym.jl
+++ src/POMDPGym.jl
@@ -1,7 +1,44 @@
 module POMDPGym
     using POMDPs 
     using OpenAIGym
+    using POMDPModelTools
+    using PyCall
+    using Random
     
+    export GymPOMDP
     
-
+    const S = PyCall.PyArray{Float64,1}
+    const A = Int64
+    const O = AbstractArray
+    
+    struct GymPOMDP <: POMDP{S, A, O}
+        env::GymEnv # OpenAIGym environment
+        pixel_observations::Bool # Whether or not to use an image as the observation
+        γ::Float64 # Discount factor
+        actions::Vector{Int64} # List of actions (Discrete only for now)
+    end
+    
+    function GymPOMDP(environment; version = :v0, pixel_observations = false, γ = 0.99, kwargs...)
+        env = GymEnv(environment, version, kwargs...)
+        s0 = reset!(env)
+        a = OpenAIGym.actions(env, s0)
+        a isa LearnBase.DiscreteSet{UnitRange{Int64}} ? (Na = length(a)) : error("GymPOMDP Currently only works with discrete action spaces. Got: $(typeof(a))")
+        GymPOMDP(env, pixel_observations, γ, collect(1:Na))
+    end
+    
+    POMDPs.initialstate(mdp::GymPOMDP, rng::AbstractRNG = Random.GLOBAL_RNG)  = ImplicitDistribution((rng) -> reset!(mdp.env))
+    
+    POMDPs.actions(mdp::GymPOMDP) = mdp.actions
+    POMDPs.actionindex(mdp::GymPOMDP, a::A) = a
+    
+    POMDPs.isterminal(mdp::GymPOMDP, s::S) = mdp.env.done
+    POMDPs.discount(mdp::GymPOMDP) = mdp.γ
+    
+    function POMDPs.gen(mdp::GymPOMDP, s::S, a::A, rng::AbstractRNG = Random.GLOBAL_RNG)
+        a_py = a - 1 # Python indexes from 0
+        r, sp = step!(mdp.env, a_py)
+        o = mdp.pixel_observations ? OpenAIGym.render(mdp.env, mode = :rgb_array) : sp
+        return (sp=sp, o=o, r=r)
+    end
 end # module
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,37 @@
 using Test
+using POMDPs
+using POMDPGym
+using POMDPSimulators, POMDPPolicies
+
+# Test MDP construction
+mdp = GymPOMDP(:CartPole, pixel_observations = true)
+
+@test mdp.pixel_observations == true
+@test mdp.γ == 0.99
+@test mdp.actions == [1,2]
+
+# Test basic POMDP interface
+@test length(rand(initialstate(mdp))) == 4
+@test actions(mdp) == [1,2]
+@test actionindex(mdp, 1) == 1
+@test actionindex(mdp, 2) == 2
+@test isterminal(mdp, rand(initialstate(mdp))) == false
+@test discount(mdp) == mdp.γ
+
+# Test gen function 
+sp, o, r = gen(mdp, mdp.env.state, 1)
+close(mdp.env)
+@test length(sp) == 4
+@test r isa Real
+@test size(o) == (400,600,3)
+
+mdp_nopix = GymPOMDP(:CartPole, pixel_observations = false)
+sp, o, r = gen(mdp_nopix, mdp_nopix.env.state, 1)
+@test o == sp
+
+# Do 1 episode
+h = simulate(HistoryRecorder(), mdp_nopix, RandomPolicy(mdp_nopix))
+@test isterminal(mdp, mdp.env.state)
+
+
+
